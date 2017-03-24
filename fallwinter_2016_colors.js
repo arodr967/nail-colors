09f84f608e59fd09345a44073a481b420bc1348b
Alicia Rodriguez Taboada <alicia_rodriguez@ultimatesoftware.com>
2017-03-24 12:57:45 -0400

    add new collection

---
+++ fallwinter_2016_colors.js
@@ -0,0 +1,19 @@
+var collection = [
+  'Inside the ISABELLEtway',
+  // 'CIA = Color is Awesome', // 01/13/17
+  'Stay Off the Lawn!!', // 11/15/16
+  'Suzi - The First Lady of Nails',
+  // 'Never a Dulles Moment', // 10/11/16
+  // 'Pale to the Chief', // 09/20/16
+  'Yank My Doodle',
+  'Freedom of Peach',
+  'Squeaker of the House',
+  'Shh…It’s Top Secret',
+  // 'Madam President', // 10/25/16
+  'OPI by Popular Vote',
+  // 'We the Female', // 01/27/17
+  // 'Kerry Blossom', // Not available
+  'Liv in the Gray'
+];
+
+console.log(collection[Math.floor((Math.random() * (collection.length)) + 0)]);
---
+++ springsummer_2017_colors.js
@@ -0,0 +1,16 @@
+var collection = [
+  'Living On the Bula-vard!',
+  'Getting Nadi On My Honeymoon',
+  // 'Polly Want a Lacquer?', // 03/24/17
+  'Do You Sea What I Sea?',
+  'Is That a Spear in Your Pocket?',
+  'I Can Never Hut Up',
+  'Super Trop-i-cal-i-fiji-istic',
+  // 'Suzi Without a Paddle', // 03/10/17
+  'Coconuts Over OPI',
+  'No Tan Lines',
+  'Exotic Birds Do Not Tweet',
+  'Two-timing the Zones'
+];
+
+console.log(collection[Math.floor((Math.random() * (collection.length)) + 0)]);
